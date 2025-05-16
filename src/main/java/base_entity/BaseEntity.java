aa34338170d6d067e8fed78a635fd650390c545a
JIN <wlsqkrtk11@naver.com>
2025-05-16 19:38:10 +0900

    # feat : baseEntity 작업

---
+++ src/main/java/base_entity/BaseEntity.java
@@ -0,0 +1,30 @@
+package base_entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+
+public class BaseEntity {
+
+    @Column(nullable = false, columnDefinition = "TINYINT(1) DEFAULT 0" )
+    private boolean withdrawn;
+
+    @CreatedDate // 생성시 자동입력
+    @Column(updatable = false)
+    private LocalDateTime creatTime;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedTime;
+}
---
+++ src/test/java/com/example/shopingplusassignment/ShopingPlusAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopingplusassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopingPlusAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
