a4764aee1b2ba732e4729ec6d34fcd4e7d07434d
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-05-19 15:42:55 +0900

    feat: comment 생성 객체 추가

--- src/main/java/com/example/shopingplusassignment/domain/comment/entity/Comment.java
+++ src/main/java/com/example/shopingplusassignment/domain/comment/entity/Comment.java
@@ -7,11 +7,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 import lombok.Getter;
 
 import com.example.shopingplusassignment.domain.comment.dto.CommentRequestDto;
+import com.example.shopingplusassignment.domain.product.entity.Product;
+import com.example.shopingplusassignment.domain.user.entity.User;
 
 @Getter
 @Entity
@@ -20,10 +24,21 @@ public class Comment extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	User user;
+
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+    Product product;
+
 	@Column(nullable = false)
 	String title;
+
 	@Column(nullable = false)
 	String description;
+
 	@Column(nullable = false)
 	int rating;
 
@@ -35,6 +50,13 @@ public class Comment extends BaseEntity {
 		this.title = dto.getTitle();
 		this.description = dto.getDescription();
 		this.rating = dto.getRating();
+	}
+	public Comment(CommentRequestDto dto,Order order){
+		this.title = dto.getTitle();
+		this.description = dto.getDescription();
+		this.rating = dto.getRating();
+		this.user = order.getUser();
+		this.product = order.getProduct();
 	}
 
 }
