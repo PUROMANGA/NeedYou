ea6d04e79d0ca1ecc90cf90abe341392cd906caf
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-05-19 10:41:07 +0900

    feat: Comment 추가

--- src/main/java/com/example/shopingplusassignment/domain/comment/dto/CommentRequestDto.java
+++ src/main/java/com/example/shopingplusassignment/domain/comment/dto/CommentRequestDto.java
@@ -12,4 +12,5 @@ public class CommentRequestDto {
 	String description;
 
 	int rating;
+
 }
--- src/main/java/com/example/shopingplusassignment/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/example/shopingplusassignment/domain/comment/repository/CommentRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.shopingplusassignment.domain.comment.entity.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
 	Optional<Comment> findById(Long id);
 
 }
