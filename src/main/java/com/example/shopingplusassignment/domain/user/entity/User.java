5723788a358673f0267a14a6d9f2cdf4525d86bc
baegjonghyeon <baek011226@naver.com>
2025-05-19 11:03:52 +0900

    refactor: User 엔티티 수정

--- src/main/java/com/example/shopingplusassignment/domain/user/entity/User.java
+++ src/main/java/com/example/shopingplusassignment/domain/user/entity/User.java
@@ -3,14 +3,19 @@ package com.example.shopingplusassignment.domain.user.entity;
 import base_entity.BaseEntity;
 import com.example.shopingplusassignment.domain.user.enums.UserRole;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
 @Entity
 @Table(name = "users")
+@NoArgsConstructor
 public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Long id;
     private String name;
     private String password;
     @Column(unique = true)
@@ -18,6 +23,15 @@ public class User extends BaseEntity {
     private String phone;
     @Enumerated(EnumType.STRING)
     private UserRole userRole;
-    private boolean status;
+    @Column(nullable = false)
+    private boolean status = false;
+
+    public User(String name, String password, String email, String phone, UserRole userRole) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.phone = phone;
+        this.userRole = userRole;
+    }
 
 }
