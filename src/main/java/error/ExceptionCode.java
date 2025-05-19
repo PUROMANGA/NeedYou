d14583752cd3332ac42b09903096e9545b472f8a
J <justonesip82@gmail.com>
2025-05-20 01:43:46 +0900

    feat: Seller 관련 예외 추가

--- src/main/java/error/ExceptionCode.java
+++ src/main/java/error/ExceptionCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 
 public enum ExceptionCode implements ErrorCode {
-    VALID_EXCEPTION(HttpStatus.BAD_REQUEST, "VALID_EXCEPTION가 발생했습니다, 인텔리제이 로그를 확인해주세요");
+    VALID_EXCEPTION(HttpStatus.BAD_REQUEST, "VALID_EXCEPTION가 발생했습니다, 인텔리제이 로그를 확인해주세요"),
+    SELLER_NOT_FOUND(HttpStatus.NOT_FOUND, "판매자를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
