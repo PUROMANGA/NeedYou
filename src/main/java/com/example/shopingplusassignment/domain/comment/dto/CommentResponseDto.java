067912c9848939d78b0c6c4e8879ef7a70c5acad
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-05-19 10:16:15 +0900

    feat: Comment 추가

---
+++ src/main/java/com/example/shopingplusassignment/domain/comment/dto/CommentResponseDto.java
@@ -0,0 +1,20 @@
+package com.example.shopingplusassignment.domain.comment.dto;
+
+import lombok.Getter;
+
+import com.example.shopingplusassignment.domain.comment.entity.Comment;
+
+@Getter
+public class CommentResponseDto {
+	Long Id;
+	String title;
+	String description;
+	int rating;
+
+	public CommentResponseDto(Comment comment) {
+		this.Id = comment.getId();
+		this.title = comment.getTitle();
+		this.description = comment.getDescription();
+		this.rating = comment.getRating();
+	}
+}
