c06ccee239f3552cb1098c546ebec3cceb4aa5a2
baegjonghyeon <baek011226@naver.com>
2025-05-19 17:52:50 +0900

    feat: UserController 유저 조회 기능 구현

--- src/main/java/com/example/shopingplusassignment/domain/user/controller/UserController.java
+++ src/main/java/com/example/shopingplusassignment/domain/user/controller/UserController.java
@@ -1,4 +1,27 @@
 package com.example.shopingplusassignment.domain.user.controller;
 
+import com.example.shopingplusassignment.domain.user.dto.request.UserUpdateRequest;
+import com.example.shopingplusassignment.domain.user.dto.response.UserResponse;
+import com.example.shopingplusassignment.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    /**
+     * 유저 조회
+     * @param userId
+     * @return
+     */
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserResponse> getUser(@PathVariable Long userId){
+        return ResponseEntity.ok(userService.getUser(userId));
+    }
+
 }
