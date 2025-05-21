171d3182eb5e80f636c4108c11c8142293d9aaab
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-05-21 19:41:23 +0900

    feat: Query DSL 추가

--- src/main/java/com/example/shopingplusassignment/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/example/shopingplusassignment/domain/comment/repository/CommentRepository.java
@@ -4,13 +4,16 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.shopingplusassignment.domain.comment.entity.Comment;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom {
 
+	@EntityGraph(attributePaths = {"user"})
 	Optional<Comment> findByIdAndDeletedAtIsNull(Long id);
 
-	Page<Comment>  findCommentByRatingBetweenAndDeletedAtIsNull(int min, int max, Pageable pageable);
+
+	// Page<Comment> findAllByRatingBetweenAndDeletedAtIsNull(int min, int max, Pageable pageable);
 
 }
