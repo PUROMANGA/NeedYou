966c94a0eab467a4b231bef1d02b037f055416a6
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-05-21 19:43:03 +0900

    feat: 생성일, 수정일 추가

--- src/main/java/com/example/shopingplusassignment/domain/comment/dto/CommentResponseDto.java
+++ src/main/java/com/example/shopingplusassignment/domain/comment/dto/CommentResponseDto.java
@@ -6,18 +6,19 @@ import com.example.shopingplusassignment.domain.comment.entity.Comment;
 
 @Getter
 public class CommentResponseDto {
-	Long Id;
-
-	String title;
-
-	String description;
-
-	int rating;
+	private Long Id;
+	private String title;
+	private String description;
+	private int rating;
+	private String createdAt;
+	private String modifiedAt;
 
 	public CommentResponseDto(Comment comment) {
 		this.Id = comment.getId();
 		this.title = comment.getTitle();
 		this.description = comment.getDescription();
 		this.rating = comment.getRating();
+		this. createdAt = comment.getCreatTime().toString();
+		this.modifiedAt = comment.getModifiedTime().toString();
 	}
 }
