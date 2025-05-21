9dcafdf3e958728195befe9e42142fc1ff71c599
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-05-21 19:42:37 +0900

    fix: service 기능 테스트 및 보완 완료

--- src/main/java/com/example/shopingplusassignment/domain/comment/service/CommentService.java
+++ src/main/java/com/example/shopingplusassignment/domain/comment/service/CommentService.java
@@ -2,12 +2,13 @@ package com.example.shopingplusassignment.domain.comment.service;
 
 import java.util.List;
 
+import com.example.shopingplusassignment.domain.comment.dto.CommentGetInfoDto;
 import com.example.shopingplusassignment.domain.comment.dto.CommentMessageResponseDto;
 import com.example.shopingplusassignment.domain.comment.dto.CommentRequestDto;
 import com.example.shopingplusassignment.domain.comment.dto.CommentResponseDto;
 
 public interface CommentService {
-       CommentResponseDto saveComment(Long orderId, Long userId, CommentRequestDto dto);
-	   List<CommentResponseDto> getCommentByRating(int minRating, int maxRating , int page, int size);
-	   CommentMessageResponseDto deleteComment(Long orderId, Long userId, Long reviewId);
+       CommentResponseDto saveComment(Long orderId, Long productId, Long userId, CommentRequestDto dto);
+	   List<CommentGetInfoDto> getCommentByRating( int min, int max , int page, int size);
+	   CommentMessageResponseDto deleteComment(Long orderId, Long productId, Long userId, Long reviewId);
 }
--- src/main/java/com/example/shopingplusassignment/domain/comment/service/CommentServiceImpl.java
+++ src/main/java/com/example/shopingplusassignment/domain/comment/service/CommentServiceImpl.java
@@ -6,80 +6,69 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import com.example.shopingplusassignment.domain.comment.dto.CommentGetInfoDto;
 import com.example.shopingplusassignment.domain.comment.dto.CommentMessageResponseDto;
 import com.example.shopingplusassignment.domain.comment.dto.CommentRequestDto;
 import com.example.shopingplusassignment.domain.comment.dto.CommentResponseDto;
 import com.example.shopingplusassignment.domain.comment.entity.Comment;
 import com.example.shopingplusassignment.domain.comment.repository.CommentRepository;
-import com.example.shopingplusassignment.domain.product.entity.Product;
+import com.example.shopingplusassignment.domain.order.entity.Order;
+import com.example.shopingplusassignment.domain.order.repository.OrderRepository;
 import com.example.shopingplusassignment.domain.product.repository.ProductRepository;
+import com.example.shopingplusassignment.domain.productOrder.entity.ProductOrder;
+import com.example.shopingplusassignment.domain.user.repository.UserRepository;
 
 @Service
 @RequiredArgsConstructor
 public class CommentServiceImpl implements CommentService {
 
 	private final CommentRepository commentRepository;
-    private final OrderRepository orderRepository;
-	/**
-	 *  Comment 생성
-	 * @param orderId
-	 * @param userId
-	 * @param dto
-	 * @return commentResponse 반환
-	 */
+	private final OrderRepository orderRepository;
+	private final ProductRepository productRepository;
+
+
 	@Transactional
 	@Override
-	public CommentResponseDto saveComment(Long orderId, Long userId, CommentRequestDto dto) {
+	public CommentResponseDto saveComment(Long orderId, Long productId, Long userId, CommentRequestDto dto) {
 
-		Order order = orderRepository.findById(orderId).orElseThrow(() -> new RuntimeException("주문이 존재하지 않습니다."));
-		if (!order.getUser().getId().equals(userId)) {
-			throw new RuntimeException("주문자와 일치하지 않습니다.");
-		}
+		Order order = orderRepository.findById(orderId).orElseThrow();
+
+
 		Comment Comment = new Comment(dto, order);
-
 		Comment saveComment = commentRepository.save(Comment);
-
 		return new CommentResponseDto(saveComment);
 	}
 
-	/**
-	 *  삭제되지 않는 별점의 범위내 결과 출력하기.
-	 * @param minRating
-	 * @param maxRating
-	 * @param page
-	 * @param size
-	 * @return Page 내 응답 리스트 출력
-	 */
+
 	@Transactional(readOnly = true)
 	@Override
-	public List<CommentResponseDto> getCommentByRating(int minRating, int maxRating, int page, int size) {
+	public List<CommentGetInfoDto> getCommentByRating( int min, int max, int page, int size) {
 
-		PageRequest pageRequest = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "createdAt"));
+		PageRequest pageRequest = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "creatTime"));
 
-		Page<Comment> comments = commentRepository.findCommentByRatingBetweenAndDeletedAtIsNull(minRating, maxRating, pageRequest);
+		Page<CommentGetInfoDto> commentResponseDtos = commentRepository.findCommentsByDynamicCondition( min,  max, pageRequest);
 
-		return comments.stream().map(CommentResponseDto::new).toList();
+		commentResponseDtos.getContent().forEach(dto -> System.out.println("💬 " + dto));
+
+		return  commentResponseDtos.getContent();
 	}
 
 	/**
-	 * Comment 삭제 , 삭제시 본인거인지 자동으로 확인 후 소프트 Delete 진행
+	 *
 	 * @param orderId
+	 * @param productId
 	 * @param userId
 	 * @param reviewId
-	 * @return 정상처리 메세지 출력
+	 * @return
 	 */
 	@Transactional
 	@Override
-	public CommentMessageResponseDto deleteComment(Long orderId, Long userId, Long reviewId) {
-		Order order = orderRepository.findById(orderId).orElseThrow(() -> new RuntimeException("주문이 존재하지 않습니다."));
-		if (!order.getUser().getId().equals(userId)) {
-			throw new RuntimeException("주문자와 일치하지 않습니다.");
-		}
-
+	public CommentMessageResponseDto deleteComment(Long orderId, Long productId, Long userId, Long reviewId) {
 
 		Comment getComment = commentRepository.findByIdAndDeletedAtIsNull(reviewId)
 			.orElseThrow(()-> new RuntimeException("리뷰가 존재하지 않습니다."));
