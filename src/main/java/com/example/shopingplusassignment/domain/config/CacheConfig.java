0eb39ec7b7582117e5408824e615ae700563651f
JIN <wlsqkrtk11@naver.com>
2025-05-22 15:55:11 +0900

    # feat : Redis 커스터마이징중

---
+++ src/main/java/com/example/shopingplusassignment/domain/config/CacheConfig.java
@@ -0,0 +1,49 @@
+package com.example.shopingplusassignment.domain.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableCaching
+
+public class CacheConfig {
+
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration defaultConfig = RedisCacheConfiguration
+                .defaultCacheConfig()
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+
+        //저장할 캐시의 설정 구조
+        Map<String, RedisCacheConfiguration> redisCacheConfigMap
+                = new HashMap<>();
+
+        //캐시의 이름과 시간을 정해줌, 추가적인 직렬화 방식을 선택하고 싶으면 jdk를 context로 설정할 것
+        redisCacheConfigMap.put("PopularProduct", defaultConfig.entryTtl(Duration.ofHours(4))
+                .disableCachingNullValues());
+
+        return RedisCacheManager.builder(connectionFactory)
+                .withInitialCacheConfigurations(redisCacheConfigMap)
+                .build();
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager("product");
+    }
+}
