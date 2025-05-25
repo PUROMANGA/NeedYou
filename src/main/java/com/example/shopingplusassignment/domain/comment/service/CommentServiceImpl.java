aae2d30bc60190fc10b12543271b48581d1ba428
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-05-25 18:10:26 +0900

    feat: 좋아요 상태 변경, 댓글수 조회, 리뷰 내용 수정 추가 style: 공백 추가

--- src/main/java/com/example/shopingplusassignment/domain/comment/service/CommentServiceImpl.java
+++ src/main/java/com/example/shopingplusassignment/domain/comment/service/CommentServiceImpl.java
@@ -2,19 +2,17 @@ package com.example.shopingplusassignment.domain.comment.service;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
 
 import com.example.shopingplusassignment.error.CustomRuntimeException;
 import com.example.shopingplusassignment.error.ExceptionCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import com.example.shopingplusassignment.domain.comment.dto.CommentGetInfoDto;
 import com.example.shopingplusassignment.domain.comment.dto.CommentMessageResponseDto;
 import com.example.shopingplusassignment.domain.comment.dto.CommentRequestDto;
 import com.example.shopingplusassignment.domain.comment.dto.CommentResponseDto;
@@ -26,6 +24,7 @@ import com.example.shopingplusassignment.domain.order.repository.OrderRepository
 import com.example.shopingplusassignment.domain.product.entity.Product;
 import com.example.shopingplusassignment.domain.product.repository.ProductRepository;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CommentServiceImpl implements CommentService {
@@ -33,8 +32,6 @@ public class CommentServiceImpl implements CommentService {
 	private final CommentRepository commentRepository;
 	private final ProductRepository productRepository;
 	private final OrderRepository orderRepository;
-	private final CommentCacheService commentCacheService;
-
 
 	@Transactional
 	@Override
@@ -42,6 +39,7 @@ public class CommentServiceImpl implements CommentService {
 
 		Order order = orderRepository.findById(orderId).orElseThrow();
         if(!order.getUser().getId().equals(userId)){
+			//System.out.println(""+userId + "user" + userId);
 			throw  new CustomRuntimeException(ExceptionCode.UNAUTHORIZED_REVIEW_ACCESS);
 		}
 		if(order.getOrderStatus().equals(OrderStatus.PENDING)){
@@ -50,6 +48,7 @@ public class CommentServiceImpl implements CommentService {
 		boolean notFound = order.getProductOrderList()
 			.stream()
 			.noneMatch(po -> po.getProductId().equals(productId));
+
 		if(notFound){
 			throw  new CustomRuntimeException(ExceptionCode.PRODUCTORDER_NOT_FOUND);
 		}
@@ -60,24 +59,42 @@ public class CommentServiceImpl implements CommentService {
 	}
 
 
-    @Cacheable(value = "comment", key = "'product:' + #productId + ':page:' + #page")
-	@Transactional(readOnly = true)
+    @Transactional(readOnly = true)
 	@Override
-	public List<CommentGetInfoDto> getCommentByRating(Long productId, int min, int max, int page, int size) {
-
+	public List<CommentResponseDto> getCommentByRating(Long productId, Long min, Long max, int page, int size) {
 		PageRequest pageRequest = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "creatTime"));
+		Page<CommentResponseDto> commentResponseDtos = commentRepository.findCommentsByDynamicCondition(productId, min,  max, pageRequest);
+		return  commentResponseDtos.getContent();
+	}
 
-		Page<CommentGetInfoDto> commentResponseDtos = commentRepository.findCommentsByDynamicCondition(productId, min,  max, pageRequest);
-		List<Long> commentIds = commentResponseDtos.stream().map(CommentGetInfoDto::getId).toList();
-		Map<Long, Long> likeCounts =commentCacheService.getLikeCounts(commentIds);
+	@Override
+	public Long getCommentCount(Long productId) {
+		return commentRepository.countByProductIdAndDeletedAtIsNull(productId);
+	}
 
-		List<CommentGetInfoDto> result = commentResponseDtos.getContent();
 
-		for (CommentGetInfoDto dto : result) {
-			Long likeCount = likeCounts.getOrDefault(dto.getId(), 0L);
-			dto.updateLikeCount(likeCount);
+
+	@Transactional
+	@Override
+	public CommentResponseDto updateComment(Long userId, Long reviewId, CommentRequestDto requestDto) {
+		Comment getComment = commentRepository.findByIdAndDeletedAtIsNull(reviewId)
+			.orElseThrow(()-> new CustomRuntimeException(ExceptionCode. COMMENT_NOT_FOUND));
+		if (!getComment.getUser().getId().equals(userId)) {
+			throw new CustomRuntimeException(ExceptionCode. UNAUTHORIZED_COMMENT_DELETE);
 		}
-		return  result;
+		getComment.updateComment(requestDto.getTitle(), requestDto.getDescription(),requestDto.getRating());
+		Comment changeComment = commentRepository.save(getComment);
+		return new CommentResponseDto(changeComment);
+	}
+
+	@Transactional
+	@Override
+	public Comment applyLikeStatus(Long userId, Long reviewId, boolean status) {
+		Comment getComment = commentRepository.findByIdAndDeletedAtIsNull(reviewId)
+			.orElseThrow(()-> new CustomRuntimeException(ExceptionCode. COMMENT_NOT_FOUND));
+
+	    getComment.increaseLikeCount(status);
+		return getComment;
 	}
 
 
@@ -89,11 +106,10 @@ public class CommentServiceImpl implements CommentService {
 			.orElseThrow(()-> new CustomRuntimeException(ExceptionCode. COMMENT_NOT_FOUND));
 
 		if (!getComment.getUser().getId().equals(userId)) {
-			throw new CustomRuntimeException(ExceptionCode. UNAUTHORIZED_COMMENT_DELETE);
+			throw new CustomRuntimeException(ExceptionCode.UNAUTHORIZED_COMMENT_UPDATE);
 		}
 
 		getComment.markAsDeleted(true, LocalDateTime.now());
-
 		return new CommentMessageResponseDto("정상처리", "정상적으로 삭제 되었습니다.");
 	}
 }
