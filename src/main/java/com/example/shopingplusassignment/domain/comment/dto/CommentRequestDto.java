57d585b3ae55ce8b4089ec26069c4a305958b97e
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-05-25 18:18:42 +0900

    fix: 요청 Request Valid작업

--- src/main/java/com/example/shopingplusassignment/domain/comment/dto/CommentRequestDto.java
+++ src/main/java/com/example/shopingplusassignment/domain/comment/dto/CommentRequestDto.java
@@ -1,16 +1,30 @@
 package com.example.shopingplusassignment.domain.comment.dto;
 
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class CommentRequestDto {
 
+	@NotBlank
 	String title;
-
+	@NotBlank
 	String description;
 
+	@Max(value = 5L)
+	@Min(value = 0L)
 	int rating;
+
+	public CommentRequestDto(String title, String description, int rating) {
+		this.title = title;
+		this.description = description;
+		this.rating = rating;
+	}
 
 }
