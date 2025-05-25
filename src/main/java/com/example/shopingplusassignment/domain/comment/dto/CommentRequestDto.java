d94c9fe490767f95afd9090396ccd180dab7107f
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-05-25 20:07:16 +0900

    feat: Valid 메세지 추가

--- src/main/java/com/example/shopingplusassignment/domain/comment/dto/CommentRequestDto.java
+++ src/main/java/com/example/shopingplusassignment/domain/comment/dto/CommentRequestDto.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -12,13 +13,15 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CommentRequestDto {
 
-	@NotBlank
-	String title;
-	@NotBlank
-	String description;
+	@NotBlank(message = "제목은 비워둘 수 없습니다.")
+	private String title;
 
-	@Max(value = 5L)
-	@Min(value = 0L)
+	@NotBlank(message = "내용은 비워둘 수 없습니다.")
+	private String description;
+
+	@NotNull(message = "평점은 필수 입력 항목입니다.")
+	@Min(value = 0, message = "평점은 최소 0 이상이어야 합니다.")
+	@Max(value = 5, message = "평점은 최대 5 이하여야 합니다.")
 	int rating;
 
 	public CommentRequestDto(String title, String description, int rating) {
@@ -26,5 +29,6 @@ public class CommentRequestDto {
 		this.description = description;
 		this.rating = rating;
 	}
+
 
 }
